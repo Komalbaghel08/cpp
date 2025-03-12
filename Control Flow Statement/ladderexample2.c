df3b3fba763d438203c5a43d33c23fc3e7124bf2
Komalbaghel08 <baghelkomal411@gmail.com>
2025-03-12 19:36:18 +0530

    new

--- Control Flow Statement/ladderexample2.c
+++ Control Flow Statement/ladderexample2.c
@@ -1,6 +1,5 @@
-#include   <iostream>
+#include <iostream>
 using namespace std;
-
 int main()
 {
 
@@ -12,16 +11,14 @@ int main()
     cout<<"Enter chemistry marks ";
     cin>>c;
 
-    if ((ph >= 0 && ph <= 100) && (m >= 0 && m <= 100) && (c >= 0 && c <= 100)){
-       // cout<<"valid number ";
+    per = (ph + m + c)/3;
+
+
+    if (ph < 0 || ph > 100 || m < 0 && m > 100  || c < 0 || c > 100)
+    {
+         cout<<"Invalid number ";
     }
-    else{
-        cout<<"invalid number\n";
-    }
-
-
-
-    if(ph < 34 && m > 34 && c > 34){
+    else if(ph < 34 && m > 34 && c > 34){
         cout<<"Supply in physics \n";
     }
     else if(m < 34 && ph > 34 && c > 34){
@@ -30,11 +27,7 @@ int main()
     else if(c < 34 && ph > 34 && m > 34){
         cout<<"Supply in chemistry\n";
     }
-
-    per = (ph + m + c)/3;
-    cout<<per<<"%\n";
-
-    if(per >= 60 && per <= 100){
+   else if(per >= 60 && per <= 100){
         cout<<"First Division \n";
      }
      else if(per >= 50 && per <= 59){
@@ -42,10 +35,9 @@ int main()
      }
      else if(per >= 35 && per <= 50)
      {
-         cout<<"Third Division \n";
+         cout<<"Third Division";
      }
      else{
-       cout<<"you are fail\n";
+       cout<<"you are fail";
      }
-
-}
+    }
