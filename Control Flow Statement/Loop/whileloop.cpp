f024e1c41a09b4020f9d87797944f66a537e6f9a
Komalbaghel08 <baghelkomal411@gmail.com>
2025-03-20 22:10:56 +0530

    new

---
+++ Control Flow Statement/Loop/whileloop.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+/*int a=1;
+while(a<=10){
+        if(a%2==0)
+    cout<<a*a<<"\n";
+    a++;
+   }*/
+
+   int pwd;
+   int c=1;
+   cout<<"enter pwd\n";
+   while(c<=3)
+   {
+       cin>>pwd;
+       if(pwd==2025)
+       {
+           cout"welcome";
+           break;
+       }
+       else
+       {
+           cout<<"\ntry again\n";
+       }
+       c++;
+   }
+   if(c==4)
+}
+
