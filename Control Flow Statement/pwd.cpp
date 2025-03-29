41e6f7702b03ebc78fdbe876e56241da939d95c7
Komalbaghel08 <baghelkomal512@gmail.com>
2025-03-29 12:33:26 +0530

    update

---
+++ Control Flow Statement/pwd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int pwd;
+    cout<<"enter pwd\n";
+    cin>>pwd;
+    if(pwd>=1000 && pwd<=9999){
+        if (pwd==2025)
+        {
+            cout<<"welcome";
+        }
+        else{
+            cout<<"bheedcome";
+        }
+    }
+}
---
+++ Loop/whileloop/table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j;
+    cout<<"Enter a no.\n";
+    cin>>i;
+    j=1;
+    while (j<=10)
+    {
+       cout<<" "<<i*j<<"\n";
+       j++;
+    }
+    
+}
